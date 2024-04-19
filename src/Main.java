8ca7e1c92fcd4368de55fb9dcb4c1797e4529e60
Giuseppe <giuseppe.riccobono@hotmail.com>
2024-04-19 16:00:23 +0200

    Esercizio 3 HashSet

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,11 @@ public class Main {
     public static void main(String[] args) {
         HashSet<String> magazzino = creazioneHashSet();
 
-        // Creazione di un oggetto HashSet dello stesso tipo e popolazione
         HashSet<String> verificaArt = new HashSet<>();
         verificaArt.add("Cuffie");
         verificaArt.add("Tastiera");
         verificaArt.add("Mouse");
 
-        // Scorrere il set e rimuovere gli elementi che sono uguali all'oggetto creato
         Iterator<String> iterator = magazzino.iterator();
         while(iterator.hasNext()) {
             String articolo = iterator.next();
@@ -25,18 +23,15 @@ public class Main {
             }
         }
 
-        // Verifica se l'HashSet è vuoto
+        //verifica se il magazzino è vuoto.
         boolean verificaMagazzino = magazzino.isEmpty();
 
-        // Stampa il risultato
         System.out.println("Il magazzino è vuoto? " + verificaMagazzino);
-        System.out.println("Il magazzino contiene: " + magazzino);
     }
 
     public static HashSet<String> creazioneHashSet() {
         HashSet<String> magazzinoAgg = new HashSet<>();
 
-        // Aggiungi elementi all'HashSet
         magazzinoAgg.add("Cuffie");
         magazzinoAgg.add("Tastiera");
         magazzinoAgg.add("Mouse");
